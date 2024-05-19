fcc8c0d90d3b7a45b70a64e104229c9de7a5128b
Elmono56 <hidalgo.pablo56@gmail.com>
2024-05-19 13:20:57 -0600

    fist exercise: alloc(n,i)

--- alloc.h
+++ alloc.h
@@ -2,17 +2,38 @@
 #define ALLOCSIZE 10000 // tamaño del espacio disponible
 
 static char allocbuf[ALLOCSIZE]; // almacenamiento para alloc
-static char *allocp = allocbuf;  // siguiente posición libre
+static char *allocp_top = allocbuf;  // siguiente posición libre desde el inicio
+static char *allocp_bottom = allocbuf + ALLOCSIZE; // siguiente posición libre desde el final
 
-char *alloc(int n) { // regresa un puntero a n caracteres
-  if (allocbuf + ALLOCSIZE - allocp >= n) {
-    allocp += n;
-    return allocp - n;
-  } else
-    return 0;
+char *alloc(int n, int i) {
+  if (i == 0) { // asignación desde el inicio (de arriba hacia abajo)
+    if (allocp_top + n <= allocp_bottom) { //hay espacio en pila
+      allocp_top += n;
+      return allocp_top - n;
+    } else
+        return 0; // no hay suficiente espacio  
+  }
+  else if (i == 1) { // asignación desde el final (de abajo hacia arriba)
+    if (allocp_bottom - n >= allocp_top) { //hay espacio en pila
+      allocp_bottom -= n;
+      return allocp_bottom;
+    } else
+        return 0; // no hay suficiente espacio
+  }
+  else { // valor de i inválido
+    return 0; 
+  }
 }
 
-void afree(char *p) { // libera la memoria apunta por p
-  if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
-    allocp = p;
+void afree(char *p, int i) { // libera la memoria apuntada por p
+  if (i == 0) { // liberación desde arriba
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE) {
+      allocp_top = p;
+    }
+  }
+  else if (i == 1) { // liberación desde abajo
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE) {
+      allocp_bottom = p + ALLOCSIZE - (allocbuf + ALLOCSIZE);
+    }
+  }
 }
